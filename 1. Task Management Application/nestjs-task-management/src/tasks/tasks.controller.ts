6965da429422cf4370489243b74baf1cd4095d3d
andreas <andreas@pts.sc>
2022-08-17 17:25:26 +0700

    create tasks

--- 1. Task Management Application/nestjs-task-management/src/tasks/tasks.controller.ts
+++ 1. Task Management Application/nestjs-task-management/src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Task } from './tasks.model';
 import { TasksService } from './tasks.service';
 
@@ -9,5 +9,13 @@ export class TasksController {
   @Get()
   getAllTasks(): Task[] {
     return this.taskService.getAllTasks();
+  }
+
+  @Post()
+  createTask(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ) {
+    return this.taskService.createTask(title, description);
   }
 }
