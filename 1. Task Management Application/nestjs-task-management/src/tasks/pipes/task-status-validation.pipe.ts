ce280018edb22f121e088585cc0edaa5ab22f840
andreas <andreas@pts.sc>
2022-08-22 23:59:13 +0700

    custom validation pipe

---
+++ 1. Task Management Application/nestjs-task-management/src/tasks/pipes/task-status-validation.pipe.ts
@@ -0,0 +1,25 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  PipeTransform,
+} from '@nestjs/common';
+import { TaskStatus } from '../task.model';
+
+export class TaskStatusValidationPipe implements PipeTransform {
+  readonly allowedStatuses = [
+    TaskStatus.OPEN,
+    TaskStatus.IN_PROGRESS,
+    TaskStatus.DONE,
+  ];
+  transform(value: any) {
+    value = value.toUpperCase();
+    if (!this.isStatusValid(value)) {
+      throw new BadRequestException(`"${value}" in an invalid status`);
+    }
+    return value;
+  }
+  private isStatusValid(status: any) {
+    const idx = this.allowedStatuses.indexOf(status);
+    return idx !== -1;
+  }
+}
--- 1. Task Management Application/nestjs-task-management/src/tasks/tasks.controller.ts
+++ 1. Task Management Application/nestjs-task-management/src/tasks/tasks.controller.ts
@@ -12,6 +12,7 @@ import {
 } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 import { Task, TaskStatus } from './task.model';
 import { TasksService } from './tasks.service';
 
@@ -50,7 +51,7 @@ export class TasksController {
   @Patch('/:id')
   updateTask(
     @Param('id') id: string,
-    @Body('status') status: TaskStatus,
+    @Body('status', TaskStatusValidationPipe) status: TaskStatus,
   ): Task {
     return this.taskService.updateTask(id, status);
   }
