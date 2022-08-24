13e62defaa5fe062e883b56aea4dfcac4ff2f3a0
andreas <andreas@pts.sc>
2022-08-24 23:08:43 +0700

    signup

--- 1. Task Management Application/nestjs-task-management/src/auth/auth.service.ts
+++ 1. Task Management Application/nestjs-task-management/src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
 
 @Injectable()
@@ -11,4 +12,12 @@ export class AuthService {
     @InjectRepository(User)
     private userRepository: Repository<User>,
   ) {}
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+    const user = new User();
+    user.username = username;
+    user.password = password;
+
+    await user.save();
+  }
 }
--- 1. Task Management Application/nestjs-task-management/src/auth/user.repository.ts
+++ 1. Task Management Application/nestjs-task-management/src/auth/user.repository.ts
@@ -1,5 +1,15 @@
 import { EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
 
 @EntityRepository(User)
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+    const user = new User();
+    user.username = username;
+    user.password = password;
+
+    await user.save();
+  }
+}
