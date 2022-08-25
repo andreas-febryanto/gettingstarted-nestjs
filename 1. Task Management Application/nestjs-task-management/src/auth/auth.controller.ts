0302e8a34c84cf058325204e7907e179510a3640
andreas <andreas@pts.sc>
2022-08-25 22:39:21 +0700

    create @GetUser decorator

--- 1. Task Management Application/nestjs-task-management/src/auth/auth.controller.ts
+++ 1. Task Management Application/nestjs-task-management/src/auth/auth.controller.ts
@@ -9,6 +9,8 @@ import {
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { GetUser } from './get-user.decorator';
+import { User } from './user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -30,7 +32,7 @@ export class AuthController {
 
   @Post('/test')
   @UseGuards(AuthGuard())
-  test(@Req() req) {
-    console.log(req);
+  test(@GetUser() user: User) {
+    console.log(user);
   }
 }
